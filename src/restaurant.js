01f58b52af3a4d9b61adf129c84ca3d23b4b138f
sowjanyachittabathini <ch.sowjanya72@gmail.com>
2021-07-13 21:40:56 -0400

    minor refactoring-5

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,23 +1,23 @@
 function showData() {
-	new Set();
-	const finalResult = transformData(data);
-	let div = document.getElementById('container');
-	finalResult.forEach((value) => {
-		div.innerHTML = `${div.innerHTML}<p class='inner' >${JSON.stringify(value)}</p>`;
-	})
+    new Set();
+    const finalResult = transformData(data);
+    let div = document.getElementById('container');
+    finalResult.forEach((value) => {
+        div.innerHTML = `${div.innerHTML}<p class='inner' >${JSON.stringify(value)}</p>`;
+    })
 }
 
 function transformData(data) {
-	const result = new Set();
-	const finalResult = [];
-	data.forEach((value) => {
-		result.add(value.name);
-	})
-	result.forEach((value) => {
-		finalResult.push(data.filter(item => {
-			return !(value !== item.name);
-		}))
-	})
+    const result = new Set();
+    const finalResult = [];
+    data.forEach((value) => {
+        result.add(value.name);
+    })
+    result.forEach((value) => {
+        finalResult.push(data.filter(item => {
+            return !(value !== item.name);
+        }))
+    })
 
-	return finalResult;
+    return finalResult;
 }
